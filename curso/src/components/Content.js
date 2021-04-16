ceac6c71b2b0ebe46f7fd663abcafd39c99cc3d3
unknown <nikols.venegas@tecsup.edu.pe>
2021-04-15 21:34:54 -0500

    2. Suma de los ejercicios - CURSO

--- curso/src/components/Content.js
+++ curso/src/components/Content.js
@@ -1,11 +1,14 @@
 import Part from './Part';
 
 export default function Content(props) {
+	const s = (cont, p) => cont + p.exercises;
+	const total = props.parts.reduce(s, 0);
 	return (
 		<div>
 			{props.parts.map((parte, index) => {
 				return <Part key={parte.id} namepart={parte.name} exercisespart={parte.exercises} />;
 			})}
+			<p>Total: {total}</p>
 		</div>
 	);
 }
